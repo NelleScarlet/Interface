1de61a86e8afe836edb279584919b0fcffb3ccc8
NelleScarlet <hg2594325@gmail.com>
2022-12-06 20:21:36 +0200

    interface

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    $("#7lb").on("click", function() {
+       window.location.href = "https://nellescarlet.github.io/7lb/";
+    });
+    $("#11lb").on("click", function() {
+        window.location.href = "https://nellescarlet.github.io/11lb/";
+    });
+    $("#9lb").on("click", function() {
+        window.location.href = "https://nellescarlet.github.io/9lb/";
+    });
+    $("#8lb").on("click", function() {
+        window.location.href = "https://nellescarlet.github.io/8lb/";
+    });
+    $("#10lb").on("click", function() {
+        window.location.href = "https://nellescarlet.github.io/10lb";
+    });
+    $("#12lb").on("click", function() {
+        window.location.href = "https://nellescarlet.github.io/12lb/";
+    });
+}
